d5bba8ccf11e702d423882eeb086a18130d2c540
cola2261 <cola2261@proton.me>
2025-11-04 19:23:43 +0500

    dm3q: Edit fox_dm3q.mk
    Signed-off-by: cola2261 <cola2261@proton.me>

--- fox_dm3q.mk
+++ fox_dm3q.mk
@@ -25,11 +25,10 @@
 OF_HIDE_NOTCH := 1
 OF_USE_GREEN_LED := 0
 OF_ALLOW_DISABLE_NAVBAR := 0
+OF_ADVANCED_SECURITY := 1
 
 # Maximum permissible splash image size (in kilobytes); do *NOT* increase
 OF_SPLASH_MAX_SIZE := 130
-
-OF_ADVANCED_SECURITY := 1
 
 # OrangeFox GUI settings
 OF_SCREEN_H := 2316
